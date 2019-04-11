460c1054fb5c2b009964a50e99941904e806a319
alenaaleyn1kova <48382053+alenaaleyn1kova@users.noreply.github.com>
2019-04-11 18:37:48 +0500

    Add files via upload

---
+++ AstronomicalCatalogClassStars/Planet.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace AstronomicalCatalogLibrary
+{
+    public class Planet
+    {
+        /// <summary>
+        /// Имя/ номер планеты
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// Размер планеты
+        /// </summary>
+        public double Radius { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("{0} {1}", Name, Radius);
+        }
+    }
+}
